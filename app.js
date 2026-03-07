c16271e5076a482eda34e1162e9e49d284a1494f
Sankalp Pawar <psankalp23cs@student.mes.ac.in>
2026-03-08 01:39:47 +0530

    re

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const path = require("path");
 const connectDB = require("./database");
+
 
 const track = require("./routes/track");
 const uploadJson = require("./routes/upload-json");
@@ -12,6 +14,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", uploadJson);
 app.use("/", uploadCsv);
